3d8c39356f2c716c5525c2ca7756236399fc414a
Ram Kumar Basak <ram.kumar.basak@gmail.com>
2026-03-26 19:30:48 +0530

    Lift the state up to App component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,21 +6,31 @@ const initalGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initalGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initalGameBoard;
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((previousBoard) => {
-      const updatedBoard = [
-        ...previousBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-
-    onSelectSquare()
-
+  for (const turn of turns) {
+    console.log('In for loop');
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
+
+  console.log("Game Board length@==" + gameBoard.length + " and value@==" + gameBoard[0][0]);
+
+  // const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((previousBoard) => {
+  //     const updatedBoard = [
+  //       ...previousBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
+
+  //   onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
@@ -29,7 +39,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
